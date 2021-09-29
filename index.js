19d2ea74b424caa6d35a077b2ad80abfe75099c4
angelsf <drifterkin_93@hotmail.com>
2021-09-29 14:30:33 -0400

    3.13: Phonebook database, step1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
+const Person = require('./models/person')
 
 app.use(cors())
 
@@ -13,56 +15,22 @@ app.use(morgan((tokens, request, response) => {
 
 app.use(express.static('build'))
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
-const generateRandom = (min, max) => {
-    return Math.random() * (max - min) + min
-}
-
-const generateId = () => {
-    const maxId = (persons.length > 0)
-        ? Math.max(...persons.map(person => person.id))
-        : 1
-
-    const randomId = generateRandom(maxId * 100, maxId);
-    return Math.floor(randomId);
-}
+let persons = []
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(p => {
+        persons = p
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
 
-    if (person) {
-        response.json(person)
-    }
-    else {
-        response.status(404).end()
-    }
+    Person.findById(id)
+        .then(person => {
+            response.json(person)
+        })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -82,7 +50,7 @@ app.get('/info', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-
+    //
     if (body.name === undefined || body.number === undefined) {
         return response.status(400).json({
             error: 'name or number missing'

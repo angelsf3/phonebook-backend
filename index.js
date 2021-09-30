172416674d35a2f275fe0a388658501a029ce99f
angelsf <drifterkin_93@hotmail.com>
2021-09-30 19:02:50 -0400

    3.17*: Phonebook database, step5

--- index.js
+++ index.js
@@ -81,6 +81,22 @@ app.post('/api/persons', (request, response, next) => {
     }
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const id = request.params.id
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
 
